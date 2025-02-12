95d544bdb284a2c0755663ea1b8388572bdee2f1
Muhmin Soliu <ayatullahmuhmin3@gmail.com>
2025-02-12 12:38:23 +0100

    update

---
+++ client/components/EventAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventAction = () => {
+  return (
+    <div>EventAction</div>
+  )
+}
+
+export default EventAction
---
+++ client/store/states/globalStates.js
@@ -0,0 +1,6 @@
+export const globalStates = {
+    event: null,
+    tickets: [],
+    ticketModal: 'scale-0',
+  }
+  
